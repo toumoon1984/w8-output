936854cbce0280e38ae33f26875eb13a06f63d4f
toumoon <toumoon@infoseek.jp>
2024-07-30 16:49:16 +0900

    240730 1650

--- html-css/assets/js/app.js
+++ html-css/assets/js/app.js
@@ -1,4 +1,42 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/js/particles.json', function() {
-    console.log('callback - particles-js config loaded');
-  });
+
+// スコープ化
+(  ()=>  {
+
+// EmailとPasswordのInputBoxのIDを取得
+const $email = document.getElementById("email");
+const $password = document.getElementById("password");
+const $submit = document.getElementById("submit");
+
+
+
+console.log(localStorage.getItem("email"));
+console.log(localStorage.getItem("password"));
+
+$email.value = localStorage.getItem("email");
+$password.value = localStorage.getItem("password")
+
+
+
+// クリックハンドラー    
+const clickHandler = () => {
+    
+    // テキストボックスのテキストデータを取得
+    let input_email = $email.value;
+    let input_password = $password.value;
+
+    console.log("Email:"+ input_email +"　　PW:"+ input_password);
+
+    localStorage.setItem("email", input_email)
+    localStorage.setItem("password", input_password)
+
+}
+
+
+// document.getElementById("submit").addEventListener("click", clickHandler01);
+document.getElementById("submit").addEventListener("click", clickHandler);
+
+
+
+
+
+}) ();
